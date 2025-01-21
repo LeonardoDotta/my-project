b53601c1dc3ddfb50638809ae9714341b864e1c7
Leonardo Dotta <leonardo.dotta@nuveto.com>
2025-01-21 16:30:39 -0300

    feature: adding the login screen with the LoginController

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [LoginController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard'); 
