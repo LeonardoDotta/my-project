92008b521bf981f8aa877de20d9bf0a1610cfe09
Leonardo Dotta <leonardo.dotta@nuveto.com>
2025-01-21 09:12:48 -0300

    feature: starting the first route

--- public/index.php
+++ public/index.php
@@ -4,14 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// Verificar se o aplicativo está em manutenção
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../vendor/autoload.php';
+// Registrar o autoloader do Composer
+require __DIR__ . '/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
+// Iniciar o Laravel e manipular a solicitação
+(require_once __DIR__ . '/../bootstrap/app.php')
     ->handleRequest(Request::capture());
