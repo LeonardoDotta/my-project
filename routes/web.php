f2e02ba5a162dbbd16b4a02d1002c0baebc16de7
Leonardo Dotta <leonardo.dotta@nuveto.com>
2025-01-28 09:43:28 -0300

    wip: adding the first styles on SideBar

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,15 @@ use App\Http\Controllers\DashboardController;
 
 Route::get('/', [LoginController::class, 'index'])->name('login');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('teste1', function () {
+    return Inertia::render('Teste1');
+});
+
+Route::get('teste2', function () {
+    return Inertia::render('Teste2');
+});
+
+Route::get('teste3', function () {
+    return Inertia::render('Teste3');
+});
