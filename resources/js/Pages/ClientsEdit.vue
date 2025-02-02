d1037bce29aa429f730de6c1dcc82f0f2a8b5d2d
LeonardoDotta <ldotta918@gmail.com>
2025-02-01 21:52:00 -0300

    wip: starting the ClientsEdit

--- resources/js/Pages/ClientsEdit.vue
+++ resources/js/Pages/ClientsEdit.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import { useClientsStore } from '../../stores/Clients'
+import { useRoute } from 'vue-router'
 
 const clientsStore = useClientsStore();
+const route = useRoute();
 
 </script>
 
